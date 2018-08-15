ffcb7a91c48a0fac47c3b0006ba31eb2f5d3b909
ThanasisFilippidis <sdi1400215@di.uoa.gr>
2018-08-15 17:34:46 +0300

    Nemo adigrafe to tutorial migamisw

--- PNapp/urls.py
+++ PNapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-from PNapp.views import welcome,Homepage
+from PNapp.views import welcome, index
 
 urlpatterns = [
     path('', welcome.as_view()),
-    path('Homepage/', Homepage.as_view()),
+    path('index/', index.as_view()),
 ]
