e1ba77b4c4dfb9baf64085b6f12a3fdca5c1f13d
Thanasis Filippidis <sdi1400215@di.uoa.gr>
2018-08-25 02:01:38 +0300

    Cleared a bit messages template code.

--- PNapp/admin.py
+++ PNapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import User, Post, Connection, Comment
+from .models import User, Post, Connection, Comment, Message
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Post)
 admin.site.register(Connection)
 admin.site.register(Comment)
+admin.site.register(Message)
