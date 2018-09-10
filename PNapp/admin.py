bfe8334388baf4b8f9d2ea4c94360dc16d1b5ce3
Thanasis Filippidis <sdi1400215@di.uoa.gr>
2018-09-10 19:55:56 +0300

    safetycommit

--- PNapp/admin.py
+++ PNapp/admin.py
@@ -1,8 +1,54 @@
 from django.contrib import admin
 from .models import User, Post, Connection, Comment, Conversation, Message
 
+# Export method
+def export_xlsx(modeladmin, request, queryset):
+    import openpyxl
+    from openpyxl.cell import get_column_letter
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = 'attachment; filename=mymodel.xlsx'
+    wb = openpyxl.Workbook()
+    ws = wb.get_active_sheet()
+    ws.title = "MyModel"
+
+    row_num = 0
+
+    columns = [
+        (u"id", 15),
+        (u"email", 128),
+        (u"password", 128),
+    ]
+
+    for col_num in xrange(len(columns)):
+        c = ws.cell(row=row_num + 1, column=col_num + 1)
+        c.value = columns[col_num][0]
+        c.style.font.bold = True
+        # set column width
+        ws.column_dimensions[get_column_letter(col_num+1)].width = columns[col_num][1]
+
+    for obj in queryset:
+        row_num += 1
+        row = [
+            obj.id,
+            obj.email,
+            obj.password,
+        ]
+        for col_num in xrange(len(row)):
+            c = ws.cell(row=row_num + 1, column=col_num + 1)
+            c.value = row[col_num]
+            c.style.alignment.wrap_text = True
+
+    wb.save(response)
+    return response
+
+export_xlsx.short_description = u"Export XLSX"
+
+# Admin's model
+class MyModelAdmin(admin.ModelAdmin):
+    actions = [export_xlsx]
+
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User, MyModelAdmin)
 admin.site.register(Post)
 admin.site.register(Connection)
 admin.site.register(Comment)
